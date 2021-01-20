2096328e84b0d1bad66954a80f0670f751476be1
Zpecena <57196930+Zpecena@users.noreply.github.com>
2021-01-20 06:23:07 +0100

    custom jscript

---
+++ Semestralka/wwwroot/js/script.js
@@ -0,0 +1,28 @@
+﻿const desc = document.getElementById('desc')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+form.addEventListener('submit', (e) => {
+    let messages = []
+    if (desc.value == '' || desc.value == null) {
+        messages.push('Descriptiom of order is Required')
+
+
+    if (desc.value.length < 15) {
+        messages.push('Order must be longer than 15 letters.')
+    }
+
+    if (desc.value.length > 100) {
+        messages.push('Order must be shorter than 100 letters.')
+    }
+
+    if (desc.value.match(/^[0-9]+$/) != null)
+    {
+        messages.push('Order Cannot be only Numbers.')
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorElement.innerText = messages.join(',')
+    }
+})
+
